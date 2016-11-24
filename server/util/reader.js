1da4e790b045f52528f44e6374b051e4657582a7
cptran777 <cptran14@gmail.com>
2016-11-23 16:37:31 -0800

    hey hey lets go

--- server/util/reader.js
+++ server/util/reader.js
@@ -14,16 +14,16 @@ module.exports = (rTree) => {
 	fs.createReadStream(path.resolve(__dirname + '/../resources/Recipients.csv'))
 		.pipe(parse({columns: true}))
 		.on('data', function(csvrow) {
-	      csvData.push(csvrow);        
-	    })
-	    .on('end',function() {
-	    	for (let x = 0; x < csvData.length; x++) {
-	    		csvData[x].coordX = -(parseFloat(csvData[x].Longitude) + 122) * 100
-	    		csvData[x].coordY = (parseFloat(csvData[x].Latitude) - 37) * 100
-	    		if (x < 10) {
-	    			console.log(csvData[x]);
-	    		}
-	    	}
-	    });
+	    csvData.push(csvrow);        
+	  })
+    .on('end',function() {
+    	for (let x = 0; x < csvData.length; x++) {
+    		csvData[x].coordX = -(parseFloat(csvData[x].Longitude) + 122) * 100
+    		csvData[x].coordY = (parseFloat(csvData[x].Latitude) - 37) * 100
+    		if (x < 10) {
+    			console.log(csvData[x]);
+    		}
+    	}
+    });
 
 };
