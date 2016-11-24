043b0f8adae522b13af3e9051317fb27eb689e86
cptran777 <cptran14@gmail.com>
2016-11-23 16:38:23 -0800

    syntax fix

--- server/util/reader.js
+++ server/util/reader.js
@@ -11,19 +11,19 @@ module.exports = (rTree) => {
 
 	let csvData = [];
 
-	fs.createReadStream(path.resolve(__dirname + '/../resources/Recipients.csv'))
-		.pipe(parse({columns: true}))
-		.on('data', function(csvrow) {
-	    csvData.push(csvrow);        
-	  })
+  fs.createReadStream(path.resolve(__dirname + '/../resources/Recipients.csv'))
+    .pipe(parse({columns: true}))
+    .on('data', function(csvrow) {
+      csvData.push(csvrow);        
+    })
     .on('end',function() {
-    	for (let x = 0; x < csvData.length; x++) {
-    		csvData[x].coordX = -(parseFloat(csvData[x].Longitude) + 122) * 100
-    		csvData[x].coordY = (parseFloat(csvData[x].Latitude) - 37) * 100
-    		if (x < 10) {
-    			console.log(csvData[x]);
-    		}
-    	}
+      for (let x = 0; x < csvData.length; x++) {
+        csvData[x].coordX = -(parseFloat(csvData[x].Longitude) + 122) * 100
+        csvData[x].coordY = (parseFloat(csvData[x].Latitude) - 37) * 100
+        if (x < 10) {
+          console.log(csvData[x]);
+        }
+      }
     });
 
 };
